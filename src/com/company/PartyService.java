83ef00b4093959fb887f90a7d210a7e456bde7c5
PovOleksandr <70759845+PovOleksandr@users.noreply.github.com>
2020-09-14 17:25:45 +0300

    Delete PartyService.java

--- src/com/company/PartyService.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-final public class PartyService {
-    static void party(Dorm dorm){
-        System.out.println("There is a party in a dorm number "+dorm.getDormNumber());
-        dorm.setCleanliness(-30);
-    }
-}
